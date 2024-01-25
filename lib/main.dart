0433ee9bff55c19676d623596c496487daec2e52
Carol <caroline.regis@devmagic.com.br>
2024-01-24 22:28:36 -0300

    WIP(*): styling confirmation page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:boleto_utils/boleto_utils.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const App());
@@ -88,6 +90,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BoletoUtils'),
+        automaticallyImplyLeading: false,
       ),
       body: SizedBox.expand(
         child: Column(
@@ -104,6 +107,15 @@ class _HomeState extends State<Home> {
   }
 }
 
+class PagamentoConcluido extends StatelessWidget {
+  const PagamentoConcluido({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
 class InfosBoleto extends StatelessWidget {
   const InfosBoleto(
     this.boletoValidado, {
@@ -114,39 +126,224 @@ class InfosBoleto extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final utcDate = boletoValidado?.vencimento?.toUtc();
+    String formatedDate = DateFormat('dd/MM/yyyy').format(utcDate!);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Confirme os dados'),
+        title: const Text('Confirme os dados de pagamento'),
+        automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
-        child: Flex(
-          direction: Axis.vertical,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    // height: 80,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.pink.shade200, Colors.white],
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Valor",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color.fromRGBO(0, 0, 0, 0.357),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: SelectableText(
+                                'R\$ ${boletoValidado?.valor}\n',
+                                style: GoogleFonts.inconsolata(
+                                    fontSize: 30,
+                                    decorationColor: Colors.purple[900],
+                                    decoration: TextDecoration.underline,
+                                    color:
+                                        const Color.fromARGB(255, 107, 67, 156),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
                 Column(
                   children: [
-                    Text("valor"),
-                    SelectableText('${boletoValidado?.valor}\n'),
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 6, right: 6, top: 8, bottom: 8),
+                      child: const Text(
+                        'Banco emissor',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 111, 114, 114),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 6, right: 6, top: 8, bottom: 0),
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.pink[50],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: SelectableText(
+                        '${boletoValidado?.bancoEmissor?.banco}\n',
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 6, right: 6, top: 8, bottom: 8),
+                      child: const Text(
+                        'Vencimento',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 111, 114, 114),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 6, right: 6, top: 8, bottom: 0),
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.pink[50],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: SelectableText(
+                        '${formatedDate}\n',
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 6, right: 6, top: 8, bottom: 8),
+                      child: const Text(
+                        'Código do boleto',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 111, 114, 114),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.only(left: 6, right: 6, top: 8, bottom: 0),
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.pink[50],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: SelectableText(
+                        '${boletoValidado?.codigoBarras}\n',
+                        style: const TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ],
                 ),
               ],
             ),
-            Text('Código do boleto'),
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  color: Colors.pink[50],
-                  borderRadius: BorderRadius.circular(10)),
-              child: SelectableText(
-                '${boletoValidado?.codigoBarras}\n',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(176, 233, 221, 255)),
+                        shape: MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Home()),
+                        );
+                      },
+                      child: const Text(
+                        'Cancelar',
+                        style: TextStyle(color: Color.fromARGB(255, 181, 0, 0)),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(176, 233, 221, 255)),
+                        shape: MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (
+                              context,
+                            ) =>
+                                    PagamentoConcluido()));
+                      },
+                      child: const Text('Confirmar Pagamento'),
+                    )
+                  ],
+                )
+              ],
+            )
             // Text('Sucesso'),
             // Container(
             //   padding: EdgeInsets.all(16),
@@ -165,9 +362,6 @@ class InfosBoleto extends StatelessWidget {
             // SelectableText('Código input:\n ${boletoValidado?.codigoInput}\n'),
             // SelectableText(
             //     'Linha digitável:\n${boletoValidado?.linhaDigitavel}\n'),
-            SelectableText(
-                'Banco emissor: ${boletoValidado?.bancoEmissor?.banco}\n'),
-            SelectableText('Vencimento: ${boletoValidado?.vencimento}\n'),
           ],
         ),
       ),
